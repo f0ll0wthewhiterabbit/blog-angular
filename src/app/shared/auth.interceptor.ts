f94be2fb8d302d8fc544a4a94a8ac363c7dd9483
followthewhiterabbit <3tonishere@gmail.com>
2020-04-26 21:11:10 +0300

    Implement auth interceptor

---
+++ src/app/shared/auth.interceptor.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core'
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+} from '@angular/common/http'
+import { Observable, throwError } from 'rxjs'
+import { Router } from '@angular/router'
+
+import { AuthService } from '../admin/shared/services/auth.service'
+import { catchError, tap } from 'rxjs/operators'
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (this.auth.isAuthenticated()) {
+      request = request.clone({
+        setParams: {
+          auth: this.auth.token,
+        },
+      })
+    }
+
+    return next.handle(request).pipe(
+      tap(() => {
+        console.log('Intercept')
+      }),
+      catchError((error: HttpErrorResponse) => {
+        console.log('[Interceptor Error]: ', error)
+        if (error.status === 401) {
+          this.auth.logout()
+          this.router.navigate(['/admin', 'login'], {
+            queryParams: {
+              authFailed: true,
+            },
+          })
+        }
+
+        return throwError(error)
+      })
+    )
+  }
+}
