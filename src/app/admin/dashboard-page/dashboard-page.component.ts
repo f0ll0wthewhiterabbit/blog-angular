d068ee7950f213659274108d93ac250b8128c83a
followthewhiterabbit <3tonishere@gmail.com>
2020-04-27 09:09:15 +0300

    Display a list of posts on the dashboard page

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { Subscription } from 'rxjs'
+
+import { PostsService } from 'src/app/shared/posts.service'
+import { Post } from 'src/app/shared/interfaces'
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
-  styleUrls: ['./dashboard-page.component.scss']
+  styleUrls: ['./dashboard-page.component.scss'],
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
+  posts: Post[] = []
+  postsSubscription: Subscription
 
-  constructor() { }
+  constructor(private postsService: PostsService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.postsSubscription = this.postsService.getAll().subscribe((posts) => {
+      this.posts = posts
+    })
   }
 
+  remove(id: string) {}
+
+  ngOnDestroy() {
+    if (this.postsSubscription) {
+      this.postsSubscription.unsubscribe()
+    }
+  }
 }
