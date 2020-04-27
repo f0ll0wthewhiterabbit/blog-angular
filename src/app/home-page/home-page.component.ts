961335e512349662118026c13838952a68044c21
followthewhiterabbit <3tonishere@gmail.com>
2020-04-27 11:22:24 +0300

    Add posts to home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Observable } from 'rxjs'
+
+import { PostsService } from '../shared/posts.service'
+import { Post } from '../shared/interfaces'
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
+  posts$: Observable<Post[]>
 
-  constructor() { }
+  constructor(private postsService: PostsService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.posts$ = this.postsService.getAll()
   }
-
 }
