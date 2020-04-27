6f98721629bb3896fa79a501a5a547f1a62b5dbd
followthewhiterabbit <3tonishere@gmail.com>
2020-04-27 11:35:17 +0300

    Implement post page

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, Params } from '@angular/router'
+import { Observable } from 'rxjs'
+import { switchMap } from 'rxjs/operators'
+
+import { PostsService } from '../shared/posts.service'
+import { Post } from '../shared/interfaces'
 
 @Component({
   selector: 'app-post-page',
   templateUrl: './post-page.component.html',
-  styleUrls: ['./post-page.component.scss']
+  styleUrls: ['./post-page.component.scss'],
 })
 export class PostPageComponent implements OnInit {
+  post$: Observable<Post>
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private postsService: PostsService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.post$ = this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.postsService.getById(params.id)
+      })
+    )
   }
-
 }
